7a76857cd4db4cb6b4d78b68ac6c956e8c7f0a19
nicolnasr <nasr528@gmail.com>
2021-01-06 17:46:19 +0200

    added teamtrees command
    added the team trees command will give the total amount of trees planted and the last 10 donators

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import datetime, time
+import requests, re, beautifulsoup
 
 def permissionCheck(author, rolename):
     rolesc = 0
@@ -176,7 +177,46 @@ class admin(commands.Cog):
       for reaction in msg.reactions:
         await msg.clear_reaction(reaction)
       await msg.unpin()
+    
+    @commands.command()
+    async def teamtrees(self, ctx):
+      page = requests.get("https://teamtrees.org/")
+      soup = BeautifulSoup(page.content, 'html.parser')
+      count = str(soup.find(id="totalTrees"))
+      p = re.compile('\d+').findall(count)
+      count = p[5]
+      count = list(count)
+      j=0
+      for i in range(len(count)-1, -1, -1):
+        j = j +1
+        if j == 3:
+          count.insert(i, ',')
+          j = 0
+      count = ''.join(count)
+      # await ctx.send(f'{count} Trees planted so far')
+      leader = soup.find_all('p', class_='text-spruce font-black text-lg')
+      trees_planted_by_peop = soup.find_all('div', class_='mt-0 md:mt-4 bg-lightMoss rounded-full text-white text-bold px-4 relative badge')
 
+      names = []
+      for name in leader:
+        names.append(name.string)
+
+      i=0
+      trees_planted = []
+      for num in trees_planted_by_peop:
+        trees_planted.append(num.string)
+      finalres = []
+      for planted in trees_planted[:10]:
+        finalres += f'\n {names[i].ljust(20)} planted: {planted}'
+        i = i + 1
+      finalres = ''.join(finalres)
+
+      embed = discord.Embed(
+                        title = f'{count} Trees planted so far',
+                        colour = ctx.author.colour,
+                        description = finalres
+                        )
+      await ctx.send(embed = embed)
 
 
 
